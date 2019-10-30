1cd6bf132b3b4a8bea55a622cff250aa4854991f
Dejan <dejan@meznarc.si>
2019-10-30 17:58:42 +0100

    changed servorefresh interval

--- src/Servo.h
+++ src/Servo.h
@@ -49,8 +49,9 @@
 #define MIN_PULSE_WIDTH       544     // the shortest pulse sent to a servo  
 #define MAX_PULSE_WIDTH      2400     // the longest pulse sent to a servo 
 #define DEFAULT_PULSE_WIDTH  1500     // default pulse width when servo is attached
-#define REFRESH_INTERVAL    10000     // minumim time to refresh servos in microseconds
+#define REFRESH_INTERVAL    30000     // minumim time to refresh servos in microseconds
 
+#warning "TO SE UPORABLA"
 #if !defined(ESP8266)
 
 #warning "This library only supports esp8266 boards."
